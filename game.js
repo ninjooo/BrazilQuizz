eaf30bb5814dfac32b7b487723cfa05ba6c1e6d3
ninjooo <62665525+ninjooo@users.noreply.github.com>
2022-02-02 18:50:51 +0200

    Add files via upload

--- game.js
+++ game.js
@@ -66,7 +66,7 @@ getNewQuestion = () => {
     if (availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
         localStorage.setItem('mostRecentScore', score)
 
-        return window.location.assign(url: 'https://ninjooo.github.io/end.html')
+        return window.location.assign('end.html')
     }
 
     questionCounter++
